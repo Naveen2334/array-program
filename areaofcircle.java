f5f1de8daea0056c81443b43dfd281808abcb348
Naveen2334 <naveenkumae16613@gmail.com>
2022-11-17 15:00:06 +0530

    ghafuujk

---
+++ areaofcircle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class areaofcircle {
+
+	public static void main(String[] args) {
+		Object system;
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(system.in);
+		System.out.println("enter the radius :");
+		int r;
+		double a = s.nextDouble();
+		double area = (22*r*r)/7;
+			System.out.println("area"+area);
+		
+
+	}
+
+}
---
+++ areaoftriangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class areaoftriangle {
+
+	public static void main(String[] args) {
+		java.util.Scanner s = new Scanner(system.in);
+		System.out.println("enter the value of r");
+		double r = s.nextDouble();
+		
+		
+		
+
+	}
+
+}
